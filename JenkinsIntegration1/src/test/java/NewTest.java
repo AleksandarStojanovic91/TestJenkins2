c04896827679c55687385b5c12c6e5898082027b
Aleksandar Stojanovic <31828736+AleksandarStojanovic91@users.noreply.github.com>
2020-11-27 19:37:56 +0100

    Add files via upload

---
+++ JenkinsIntegration1/src/test/java/NewTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+    private WebDriver driver;
+    @Test
+    public void testEasy() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+    }
+    @BeforeTest
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
